af0b1be9bcee0d769c15ed9ebdf6e606255a5c3e
tsvalencio-IA <tsvalencio@gmail.com>
2026-09-09 14:59:01 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,12 @@
 // ===============================================================
 
 export const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI.firebaseapp.com",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI.firebasestorage.app",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyArXXVD0QnJvDdB9QVNc4QrBNTv-h7HtfQ",
+  authDomain: "ottofest-3ca4e.firebaseapp.com",
+  projectId: "ottofest-3ca4e",
+  storageBucket: "ottofest-3ca4e.firebasestorage.app",
+  messagingSenderId: "108804464983",
+  appId: "1:108804464983:web:27e7dc21ec214876315e12"
 };
 
 // Firestore é usado para:
